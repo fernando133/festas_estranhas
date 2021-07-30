02ca307e108ef784ab1e202088388923b0b6b941
Fernando Gualberto <fernando.gmp@gmail.com>
2021-07-29 21:27:04 -0300

    adicionando modelo de convidado e .gitignore

--- festas_estranhas/convidados/models.py
+++ festas_estranhas/convidados/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Convidado(models.Model):
+	id = models.BigAutoField(primary_key=True)
+	nome = models.CharField(
+		'Nome',
+		help_text  = 'Nome completo',
+		max_length = 200
+	)
+
+	e_mail = models.CharField(
+		'E-mail',
+		max_length = 300,
+		help_text= 'Informe e-mail'
+	)
+
+	telefone = models.CharField(
+		'Telefone',
+		max_length = 300,
+		help_text= 'Informe e-mail'
+	)
+
+	def __str__(self):
+		return self.nome
+	
+	class Meta:
+		verbose_name        = "Convidado"
+		verbose_name_plural = "Convidados"
