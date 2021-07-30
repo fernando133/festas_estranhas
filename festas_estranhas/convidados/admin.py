a97e12954429b314b849104df2a1e83d524ffe50
Fernando Gualberto <fernando.gmp@gmail.com>
2021-07-29 21:40:47 -0300

    registrando modelo de convidado

--- festas_estranhas/convidados/admin.py
+++ festas_estranhas/convidados/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Convidado
 
-# Register your models here.
+@admin.register(Convidado)
+class ConvidadoInline(admin.ModelAdmin):
+	list_display = ['id', 'nome', 'e_mail', 'telefone', 'confirmou_presenca']
+	search_fields = ['nome', 'e-mail']
+	list_filter   = ('confirmou_presenca',)
+
