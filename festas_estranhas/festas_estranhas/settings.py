6b3e8799b291e607de970cd691a2be1b25ad728b
fernando133 <fernando.gmp@gmail.com>
2021-07-30 20:25:46 -0300

    adicionando apps eventos e django_filters

--- festas_estranhas/festas_estranhas/settings.py
+++ festas_estranhas/festas_estranhas/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'eventos',
     'convidados',
+    'django_filters'
 ]
 
 MIDDLEWARE = [
