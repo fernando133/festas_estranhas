866927cae3691a568eeca79dd3f7adeeccaf8a23
fernando133 <fernando.gmp@gmail.com>
2021-08-01 01:14:03 -0300

    atualizando atributo de data_hora

--- festas_estranhas/eventos/admin.py
+++ festas_estranhas/eventos/admin.py
@@ -3,6 +3,6 @@ from .models import Evento
 
 @admin.register(Evento)
 class EventoInline(admin.ModelAdmin):
-	list_display = ['id', 'descricao', 'data']
+	list_display = ['id', 'descricao','responsavel', 'data_hora']
 	search_fields = ['descricao']
-	list_filter   = ('data',)
+	list_filter   = ('data_hora',)
