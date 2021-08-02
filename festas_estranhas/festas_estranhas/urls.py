513fe092a48de0e8a3a7684d7a6ae37d1fca82e3
fernando133 <fernando.gmp@gmail.com>
2021-08-02 00:41:53 -0300

    adicionando django rest framework jwt

--- festas_estranhas/festas_estranhas/urls.py
+++ festas_estranhas/festas_estranhas/urls.py
@@ -6,6 +6,7 @@ from rest_framework import viewsets
 from rest_framework.authtoken import views
 from eventos.api.viewsets import EventoViewSet
 from rest_framework.authtoken.models import Token
+from rest_framework_simplejwt import views as jwt_views
 from convidados.api.viewsets import ConvidadoViewSet, ConfirmarPresencaViewSet
 
 router = routers.DefaultRouter()
@@ -21,4 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api-token-auth/', views.obtain_auth_token),
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
