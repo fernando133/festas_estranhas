72b40af08e22e24f3c466fb74d75eec33cb463a9
fernando133 <fernando.gmp@gmail.com>
2021-08-01 22:31:32 -0300

    adicionando authtoken

--- festas_estranhas/festas_estranhas/urls.py
+++ festas_estranhas/festas_estranhas/urls.py
@@ -3,8 +3,10 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework import routers
 from rest_framework import viewsets
-from convidados.api.viewsets import ConvidadoViewSet
+from rest_framework.authtoken import views
 from eventos.api.viewsets import EventoViewSet
+from rest_framework.authtoken.models import Token
+from convidados.api.viewsets import ConvidadoViewSet
 
 router = routers.DefaultRouter()
 #rotas app convidados
@@ -17,4 +19,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('api-token-auth/', views.obtain_auth_token),
 ]
